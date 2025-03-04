2d9c713b4d1442e41df7caeb96a204d12b8e7036
caixiaojie <464488608@qq.com>
2025-03-05 00:20:52 +0800

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  base: '/cai_duting/', // 确保正确指向仓库路径
-})
