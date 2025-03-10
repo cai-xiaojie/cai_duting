add94569169eb6569988734751249368da79a219
caixiaojie <464488608@qq.com>
2025-03-10 16:25:12 +0800

    新

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/cai_duting/', // 添加这一行，确保正确指向仓库路径
+  base: '/', // 修改为根路径
 })
